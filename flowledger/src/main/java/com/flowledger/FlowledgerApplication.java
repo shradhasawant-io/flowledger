1a0eac1d0186610d642e65db86d0eba44ad92e47
Shradha Sawant <shraddha888sawant@gmail.com>
2026-07-08 17:51:51 +0530

    Initial Spring Boot project setup

---
+++ flowledger/src/main/java/com/flowledger/FlowledgerApplication.java
@@ -0,0 +1,13 @@
+package com.flowledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowledgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowledgerApplication.class, args);
+	}
+
+}
---
+++ flowledger/src/test/java/com/flowledger/FlowledgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowledgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
